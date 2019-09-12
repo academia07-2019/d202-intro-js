415a97351b2f33fc7001c4d5aa4a0e878a034bba
Ana Beatriz Morita <anabemorita@gmail.com>
2019-09-12 08:18:02 -0300

    adicionar esxplicações de js

---
+++ script.js
@@ -0,0 +1,72 @@
+//DECLARANDO VARIAVEIS
+
+var variavel1;
+let variavel2 ; 
+const variavel3 = 12; //constantes precisam ter um valor atribuido quando declaradas
+
+//atribuiçao
+
+var variavel4 = 12;
+let variavel5 = "estação hack"; 
+const variavel6 = true;
+
+//tipos de dados
+
+let exemplo;
+exemplo = `Ana`; // String 
+exemplo = 23; // Number
+exemplo = 1.57;// Number
+exemplo = true; // Boolean
+exemplo = false; // Boolean
+
+let saboresDePizza= [];// Lista
+
+saboresDePizza = ["brocolis", "baiana", "portuguesa", "mussarela", "strogonoff"]; // Lista
+
+let fagner = ["Mauricio", 18, true, "Santana", ["pé de moleque", "mousse", "pudim"] ]; //Lista
+
+let fayra = {
+    sobrenome:'Miranda',
+    idade: 25,
+    viva: true,
+    cidade: "Osasco",
+    hobbies: ["dança", "desenho", "leitura"]
+}; //Objeto
+
+let barbara = {
+    sobrenome: "Moreira",
+    idade: 19,
+    viva: true,
+    bairro: "Freguesia",
+    hobbies: ['otaku', 'dorameira', 'capopeira'],
+    amiga: fayra
+}; //Objeto
+
+//Estruturas
+
+//Condicao
+
+    // if(condicao){
+    //     codigo executado quando a condicao é verdadeira
+    // }else{
+    //     codigo a ser executado se a condicao for falsa
+    // }
+    // if(condicao){
+       // codigo executado quando a condicao é verdadeira
+    // }else if(condicao){
+        //codigo a ser executado se a primeira condicao for falsa e a segunda for verdadeira
+    // }else{
+        // codigo a ser executado quando todas as condicoes forem falsas
+    // }
+
+//entradas do navegador
+    // prompt
+//saida do navegador
+    // alert
+    // console.log
+
+    //Estrutura de Repeticao
+    //enquanto
+    //while(condicao){
+        //codigo a ser repetido
+   // }
